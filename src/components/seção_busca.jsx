baea122c9c1e0d70d6c5c15fe1239f4658f46ade
rafael-ulguim1 <rv862086@gmail.com>
2026-04-16 18:59:07 -0300

    feat: cria estrutura base do card de busca

--- src/components/seção_busca.jsx
+++ src/components/seção_busca.jsx
@@ -1,16 +1,25 @@
 function Seção_busca_main() {
     return (
-        <section id="busca" className="flex justify-center">
+        <section id="busca" className="flex justify-center bg-gray-500">
             <div>
+                <h1> Busque por SUV até 100 mil</h1>
 
             </div>
             <div>
+                <button> Buscar carros</button>
+                <button> Buscar motos</button>
 
             </div>
             <div>
+                <input
+                    type="text"
+                    placeholder="Descreva oque voce está procurando"
+                    className=""
+                />
 
             </div>
             <div>
+                <button className="bg-red-500 text-white"> Ver todas as ofertas (400.000)</button>
 
             </div>
             <div>
