91bb28766c8ebfebc6f0d47664d60942a4e86748
rafael-ulguim1 <rv862086@gmail.com>
2026-04-16 19:54:14 -0300

    style: melhora layout do card de busca

--- src/components/seção_busca.jsx
+++ src/components/seção_busca.jsx
@@ -1,31 +1,32 @@
 function Seção_busca_main() {
     return (
-        <section id="busca" className="flex justify-center bg-gray-500">
-            <div>
-                <h1> Busque por SUV até 100 mil</h1>
+        <section id="busca" className="flex justify-center   ">
+            <div className="bg-gray-500 h-72 w-250 flex flex-col gap-3 rounded shadow ">
 
-            </div>
-            <div>
-                <button> Buscar carros</button>
-                <button> Buscar motos</button>
 
-            </div>
-            <div>
-                <input
-                    type="text"
-                    placeholder="Descreva oque voce está procurando"
-                    className=""
-                />
+                <div className="mt-5 ml-10 font-bold text-2xl text-gray-300">
+                    <h1> Busque por SUV até 100 mil</h1>
 
-            </div>
-            <div>
-                <button className="bg-red-500 text-white"> Ver todas as ofertas (400.000)</button>
+                </div>
+                <div className=" ml-10 flex gap-4 font-bold">
+                    <button className="text-gray-100 bg-gray-700 rounded-full h-9 w-30"> Buscar carros</button>
+                    <button className="text-gray-100 bg-gray-700 rounded-full h-9 w-30"> Buscar motos</button>
 
-            </div>
-            <div>
+                </div>
+                <div className=" ml-10">
+                    <input
+                        type="text"
+                        placeholder=" 🚗 Descreva oque voce está procurando"
+                        className="bg-white rounded-full h-15 w-230 border-2 border-purple-700   "
+                    />
 
-            </div>
-            <div>
+                </div>
+                <div className="flex-1 text-center mt-5">
+                    <button className="bg-red-500 h-10 w-70 text-white font-bold  rounded"> Ver todas as ofertas (400.000)</button>
+
+                </div>
+
+
 
             </div>
 
