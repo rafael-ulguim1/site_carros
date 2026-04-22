be11a72fce2b83829b657b5ec828f8916dc5b4d3
rafael-ulguim1 <rv862086@gmail.com>
2026-04-22 16:40:01 -0300

    style: cards da seção categorias prontos

--- src/components/categorias.jsx
+++ src/components/categorias.jsx
@@ -1,74 +1,78 @@
 function Categorias() {
     return (
-        <section id="soluçoes" className="mt-1 flex justify-center flex-col  ">
+        <section id="soluçoes" className="mt-10 flex justify-center flex-col  ">
             <div className="flex ml-80">
                 <h1 className="font-bold text-gray-600 text-xl"> Categorias </h1>
             </div>
-            <div className=" flex gap-5 mt-4 justify-center">
-                <button className="bg-white h-50 w-58 rounded-2xl border-1 border-gray-300">
-                    <div className="flex-1 text-left ml-4 flex flex-col gap-2  ">
-                        <img
-                            src="/src/assets/soluções1.jpg"
-                            alt="soluções1"
-                            className="w-12 h-12 object-covers  rounded-full  "
-                        />
-                        <h1 className="font-bold text-xl"> Carros por assinatura </h1>
-                        <p className="font-serif text-gray-600 text-sm"> Compare e encontre as melhores ofertas </p>
+            <div className=" flex gap-5 mt-4 justify-center mb-7">
+                <button className="bg-white h-50 w-58 rounded-2xl border-1 border-gray-300"
+                    style={{
+                        backgroundImage: "url('/src/assets/eletricos.jpg')",
+                        backgroundSize: 'cover',      // Faz a imagem cobrir todo o espaço sem sobrar buracos
+                        backgroundRepeat: 'no-repeat'
 
+                    }}
+                >
+                    <div className="flex-1 text-left  ml-4 flex flex-col gap-2 ">
+                        <h1 className="font-serif text-white text-2xl font-bold w-10 mt-30"> Carros elétricos </h1>
 
                     </div>
 
                 </button>
-                <button className="bg-white h-50 w-58 rounded-2xl border-1 border-gray-300" >
+                <button className="bg-white h-50 w-58 rounded-2xl border-1 border-gray-300"
+                    style={{
+                        backgroundImage: "url('/src/assets/hatches.jfif')",
+                        backgroundSize: 'cover',      // Faz a imagem cobrir todo o espaço sem sobrar buracos
+                        backgroundRepeat: 'no-repeat'
+
+                    }}
+                >
                     <div className="flex-1 text-left  ml-4 flex flex-col gap-2 ">
-                        <img
-                            src="/src/assets/soluçoes2.png"
-                            alt="soluções1"
-                            className="w-12 h-12 object-covers  rounded-full "
-                        />
-
-                        <h1 className="font-bold text-xl"> Serviços Automativos </h1>
-                        <p className="font-serif text-gray-600 text-sm"> funilaria, manutenção e mais em oficinas perto de você </p>
-                    </div>
-
-
-                </button>
-                <button className="bg-white h-50 w-58 rounded-2xl border-1 border-gray-300">
-                    <div className="flex-1 text-left  ml-4 flex flex-col gap-2 ">
-                        <img
-                            src="/src/assets/soluções3.png"
-                            alt="soluções1"
-                            className="w-12 h-12 object-covers  rounded-full  "
-                        />
-                        <h1 className="font-bold text-xl"> Vender </h1>
-                        <p className="font-serif text-gray-600 text-sm"> Vendar facil e rápido. Anuncie para milhoes e feche o melhor negócio </p>
-
-                    </div>
-
-                </button>
-                <button className="bg-white h-50 w-58 rounded-2xl border-1 border-gray-300">
-                    <div className="flex-1 text-left  ml-4 flex flex-col gap-2 ">
-                        <img
-                            src="/src/assets/soluções4.png"
-                            alt="soluções1"
-                            className="w-12 h-12 object-covers  rounded-full "
-                        />
-                        <h1 className="font-bold text-xl"> Financiamento </h1>
-                        <p className="font-serif text-gray-600 text-sm">Aproveite milhares de ofertas com parcelas que cabem no seu bolso.</p>
+                        <h1 className="font-serif text-white text-2xl font-bold w-10 mt-30"> Hatches </h1>
 
                     </div>
 
 
                 </button>
-                <button className="bg-white h-50 w-58 rounded-2xl border-1 border-gray-300">
+                <button className="bg-white h-50 w-58 rounded-2xl border-1 border-gray-300"
+                    style={{
+                        backgroundImage: "url('/src/assets/picapes.jpg')",
+                        backgroundSize: 'cover',      // Faz a imagem cobrir todo o espaço sem sobrar buracos
+                        backgroundRepeat: 'no-repeat'
+
+                    }}
+                >
                     <div className="flex-1 text-left  ml-4 flex flex-col gap-2 ">
-                        <img
-                            src="/src/assets/soluções4.png"
-                            alt="soluções1"
-                            className="w-12 h-12 object-covers  rounded-full "
-                        />
-                        <h1 className="font-bold text-xl"> Financiamento </h1>
-                        <p className="font-serif text-gray-600 text-sm">Aproveite milhares de ofertas com parcelas que cabem no seu bolso.</p>
+                        <h1 className="font-serif text-white text-2xl font-bold w-10 mt-30"> Picapes </h1>
+
+                    </div>
+
+                </button>
+                <button className="bg-white h-50 w-58 rounded-2xl border-1 border-gray-300"
+                    style={{
+                        backgroundImage: "url('/src/assets/sedans.jfif')",
+                        backgroundSize: 'cover',      // Faz a imagem cobrir todo o espaço sem sobrar buracos
+                        backgroundRepeat: 'no-repeat'
+
+                    }}
+                >
+                    <div className="flex-1 text-left  ml-4 flex flex-col gap-2 ">
+                        <h1 className="font-serif text-white text-2xl font-bold w-10 mt-30"> Sedans </h1>
+
+                    </div>
+
+
+                </button>
+                <button className="bg-white h-50 w-58 rounded-2xl border-1 border-gray-300"
+                    style={{
+                        backgroundImage: "url('/src/assets/suvs.jfif')",
+                        backgroundSize: 'cover',      // Faz a imagem cobrir todo o espaço sem sobrar buracos
+                        backgroundRepeat: 'no-repeat'
+
+                    }}
+                >
+                    <div className="flex-1 text-left  ml-4 flex flex-col gap-2 ">
+                        <h1 className="font-serif text-white text-2xl font-bold w-10 mt-30"> Suvs </h1>
 
                     </div>
 
